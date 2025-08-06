6e1184b658cdb3bfe1c8419cbcb90b90f6d6efdd
Siddhi-Gaikwad7854 <Siddhi.Gaikwad23@iccs.ac.in>
2025-08-06 11:44:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
+<html>
+<head>
+<title>
+Additon
+</title>
+</head>
+<body>
+<form action=Addition.php, method="GET">
+Enter first number<input type="number" name="t1"><br>
+Enter second number<input type="number" name="t2"><br>
+Submit<input type="Submit" name="t3">
+</form>
+<body>
+</html>
 
-echo "Siddhi Gaikwad";
+  <?php>
+    if (isset($_GET['submit']))
+   {
+    $num1=$_GET['t1'];
+    $num2=$_GET['t2'];
+  $ans=$num1+$num2;
+  echo('Addition is :'.$ans)
+   }
+  ?>
